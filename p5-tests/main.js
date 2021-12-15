6182c4fdd25addeabb41597077006d4b34a52f1e
Dirk Krause <dirkk0@googlemail.com>
2021-12-15 19:54:58 +0100

    Update main.js

--- p5-tests/main.js
+++ p5-tests/main.js
@@ -4,10 +4,19 @@ function setup() {
 
 function draw() {
   // background(220)
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
+  // if (mouseIsPressed) {
+  //   fill(0);
+  // } else {
+  //   fill(255);
+  // }
+  // ellipse(mouseX, mouseY, 80, 80);
+
+  let dx = Math.random() * 200;
+  let dy = Math.random() * 300;
+  for (let i = 0; i < 10; i++) {
+    ellipse(50 + dx, 50 + dy, 80, 80);
   }
-  ellipse(mouseX, mouseY, 80, 80);
+  // ellipse(50, 50, 80, 80);
+  // ellipse(80, 80, 80, 80);
+  // ellipse(120, 120, 80, 80);
 }
