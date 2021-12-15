c0a2944a56ea5fde50af44da94f0a5f9558383ef
Dirk Krause <dirkk0@googlemail.com>
2021-12-15 20:38:59 +0100

    v

--- p5-tests/test2/main.js
+++ p5-tests/test2/main.js
@@ -4,39 +4,39 @@ let dy;
 let vx = 1.5;
 let vy = 1.5;
 
-let canvasWidth = 400
-let canvasHeight = 300
+let canvasWidth = 200;
+let canvasHeight = 300;
+
+let dw = 15;
 
 function setup() {
+  // frameRate(5);
   createCanvas(canvasWidth, canvasHeight);
   dx = canvasWidth / 2;
   dy = canvasHeight / 2;
-
-
 }
 
-function mousePressed () {
-  vy = -vy
+function mousePressed() {
+  vy = -vy;
 }
 
 function draw() {
   background(120);
-  // dx *= 1.02
 
-  if (dx > canvasWidth) {
-    vx = - vx
+  if (dx > canvasWidth - dw) {
+    vx = -vx;
   }
-  if (dx < 0) {
-    vx = - vx
+  if (dx < 0 + dw) {
+    vx = -vx;
   }
-  if (dy > canvasHeight) {
-    vy = - vy
+  if (dy > canvasHeight - dw) {
+    vy = -vy;
   }
-  if (dy < 0) {
-    vy = - vy
+  if (dy < 0 + dw) {
+    vy = -vy;
   }
 
-  dx += vx
-  dy += vy
-  ellipse(dx, dy, 20, 20);
+  dx += vx;
+  dy += vy;
+  ellipse(dx, dy, dw * 2, dw * 2);
 }
