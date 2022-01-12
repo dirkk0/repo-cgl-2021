4cd56ccf6d65e40480f1b29c0448fabb86865559
Dirk Krause <dirkk0@googlemail.com>
2022-01-12 18:37:08 +0100

    moar

--- p5-tests/test3/main.js
+++ p5-tests/test3/main.js
@@ -1,63 +1,55 @@
+let x = 100;
+let y = 100;
 let dx;
 let dy;
 
 let vx = 1.5;
 let vy = 1.5;
 
+let dw = 15;
+
 let canvasWidth = 250;
 let canvasHeight = 300;
 
-let dw = 15;
-
-function setup() {
-  // frameRate(5);
-  createCanvas(canvasWidth, canvasHeight);
-  dx = canvasWidth / 2;
-  dy = canvasHeight / 2;
-}
-
-function mousePressed() {
-  vy = -vy;
-}
-
-function draw() {
-  background(120);
-
-  if (dx > canvasWidth - dw) {
-    vx = -vx;
-  }
-  if (dx < 0 + dw) {
-    vx = -vx;
-  }
-  if (dy > canvasHeight - dw) {
-    vy = -vy;
-  }
-  if (dy < 0 + dw) {
-    vy = -vy;
-  }
-
-  dx += vx;
-  dy += vy;
-  ellipse(dx, dy, dw * 2, dw * 2);
-}
-
+// create an instance mode object for p5js
 let sketch = function (p5) {
-  let x = 100;
-  let y = 100;
 
-  p.setup = function () {
+  p5.mousePressed = function() {
+    vy = -vy;
+  }
+  
+
+  p5.setup = function () {
     // frameRate(5);
-    createCanvas(canvasWidth, canvasHeight);
+    p5.createCanvas(canvasWidth, canvasHeight);
     dx = canvasWidth / 2;
     dy = canvasHeight / 2;
     // p.createCanvas(700, 410);
   };
 
-  p.draw = function () {
-    p.background(0);
-    p.fill(255);
-    p.rect(x, y, 50, 50);
+  p5.draw = function () {
+    p5.background(120);
+
+    if (dx > canvasWidth - dw) {
+      vx = -vx;
+    }
+    if (dx < 0 + dw) {
+      vx = -vx;
+    }
+    if (dy > canvasHeight - dw) {
+      vy = -vy;
+    }
+    if (dy < 0 + dw) {
+      vy = -vy;
+    }
+
+    dx += vx;
+    dy += vy;
+    p5.ellipse(dx, dy, dw * 2, dw * 2);
   };
 };
+
+// create instance mode
+// https://p5js.org/examples/instance-mode-instantiation.html
 
 let myp5 = new p5(sketch);
