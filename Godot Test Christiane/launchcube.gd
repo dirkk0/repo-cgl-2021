fdd4a07a9fb40686cd75b1e9421416a76a432080
christiane-e <ch.ebrecht@gmail.com>
2022-10-26 20:29:59 +0200

    Christiane has issues

---
+++ Godot Test Christiane/launchcube.gd
@@ -0,0 +1,12 @@
+extends Spatial
+
+export var inc = 0.08
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	get_node("UpCube").translation.y += inc
+	pass
