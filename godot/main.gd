6d241fbaff85585437c549ddead611b6257a3c1c
Dirk Krause <dirkk0@googlemail.com>
2022-10-26 19:33:21 +0200

    vv

---
+++ godot/main.gd
@@ -0,0 +1,22 @@
+extends Spatial
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("hello world")
+	get_node("Cube").translation.y = 8
+	# $Cube
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func doSomething():
+	pass
