827a72038b4e1ff53ef41ac4afba7f4259a546f4
Dirk Krause <dirkk0@googlemail.com>
2022-11-02 19:29:07 +0100

    with model

--- godot/player.gd
+++ godot/player.gd
@@ -1,31 +1,43 @@
 extends KinematicBody
 
-var inc = 0.0
+var velocity:Vector3
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var acceleration = 5
+var speed = 10
+var mouse_sensitivity = 0.3
+var gravity = 0.98
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 func _physics_process(delta):
-	if Input.is_action_just_pressed("ui_w"):
-		print("you hit W")
-
-	if Input.is_action_just_pressed("ui_right"):
-		inc = 0.2
-	if Input.is_action_just_pressed("ui_left"):
-		inc = -0.2
+	var aim = get_node("Head/Camera").get_camera_transform().basis
+	var direction:Vector3 = Vector3.ZERO
+	if Input.is_action_just_pressed("ui_cancel"):
+		# Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		get_tree().quit()
+	if Input.is_action_pressed("ui_w"):
+		direction = -aim.z
+	if Input.is_action_pressed("ui_s"):
+		direction = aim.z
+	if Input.is_action_pressed("ui_a"):
+		direction = -aim.x
+	if Input.is_action_pressed("ui_d"):
+		direction = aim.x
 	if Input.is_action_just_pressed("ui_select"):
-		inc = 0
+		pass
 	
-	translation.x += inc
-	pass
+	direction = direction.normalized()
+	velocity = velocity.linear_interpolate(direction * speed, acceleration * delta)
+	velocity.y = velocity.y - gravity
+	move_and_slide(velocity)
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		var cam = get_node("Head/Camera")
+		self.rotate_y(deg2rad(-event.relative.x * mouse_sensitivity))
+		cam.rotate_x(deg2rad(-event.relative.y * mouse_sensitivity))
+		cam.rotation.x = clamp(cam.rotation.x, -PI*0.3, PI*0.3)
+	
