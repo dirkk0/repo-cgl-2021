2b55bca612c8e319a8b422614a75411466192e94
Dirk Krause <dirkk0@googlemail.com>
2022-11-09 20:47:09 +0100

    latest

--- godot/models/level1/agent.gd
+++ godot/models/level1/agent.gd
@@ -7,22 +7,24 @@ var is_on_ground = false
 var is_scanning = false
 var is_following = false
 
+var player_pos
+var agent_pos
+
+onready var my_label = get_node("Label3D")
+
+
 func _ready():
 	pass # Replace with function body.
-
-func _physics_process(odelta):
-	var direction:Vector3 = Vector3(0.0, 0.0, 0.0) 
-	# Vector3.ZERO
-	var velocity:Vector3 = Vector3.ZERO
 	
-
+func think():
+	pass
 	# var player = get_node("/root/Main/Player")
 	var player = get_node("%Player")
 	# print(player.translation)
 	get_node("%Label").text = str(player.translation)
 	
-	var player_pos = player.translation
-	var agent_pos = self.translation
+	player_pos = player.translation
+	agent_pos = self.translation
 	
 	var body = get_node("GroundRayCast").get_collider()
 	if body:
@@ -30,7 +32,6 @@ func _physics_process(odelta):
 			# print(body.is_in_group("ground"))
 			is_on_ground = true
 
-	var my_label = get_node("Label3D")
 	
 	#  is_scanning:
 	body = get_node("Nose/LookRayCast").get_collider()
@@ -46,21 +47,33 @@ func _physics_process(odelta):
 		is_following = false
 
 	
-	if is_on_ground == true:
+	if is_on_ground == true and not is_following:
 		is_scanning = true
 		pass
 
+
+	if is_scanning:
+		my_label.text = "I am scanning!"
+		self.rotate_y(0.1)
+		
+
+func _physics_process(odelta):
+	var direction:Vector3 = Vector3(0.0, 0.0, 0.0) 
+	# Vector3.ZERO
+	var velocity:Vector3 = Vector3.ZERO
+	
 	if is_following:
 		my_label.text = "I am following you!"
 		# chase the player
 		velocity = -(agent_pos - player_pos)
 		is_scanning = false
 
-	if is_scanning:
-		my_label.text = "I am scanning!"
-		self.rotate_y(0.1)
-		
+	think()
+	
 	velocity.y += gravity
 	move_and_slide(velocity)
-	
-	
+
+func _on_Timer_timeout():
+	print("timer")
+	# think()
+	pass # Replace with function body.
