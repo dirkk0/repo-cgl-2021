7951822726a259962dca923edbe303423bb5532c
Dirk Krause <dirkk0@googlemail.com>
2022-11-16 18:27:19 +0100

    various

---
+++ godot-test/main.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+
+func _ready():
+	print(Global.other_counter)
+	pass # Replace with function body.
--- godot-test/start.gd
+++ godot-test/start.gd
@@ -1,22 +1,17 @@
 extends Node2D
 
+# var click_counter = 0
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 
 func _on_Button_pressed():
 	print("button is pressed")
 	get_tree().change_scene("res://main.tscn")
+	pass # Replace with function body.
+
+
+func _on_ClickButton_pressed():
+	Global.other_counter += 1
+	get_node("ClickButton").text = str(Global.other_counter) + " clicks!"
 	pass # Replace with function body.
